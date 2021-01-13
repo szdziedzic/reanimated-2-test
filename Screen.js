2638f4c53bcba57cfdbf614ce8b47a0020a9c15a
szdziedzic <sz.dziedz@gmail.com>
2021-01-13 16:55:30 +0100

    Added position fix

--- Screen.js
+++ Screen.js
@@ -6,6 +6,7 @@ import Animated, {
 } from 'react-native-reanimated';
 import {PanGestureHandler} from 'react-native-gesture-handler';
 import React from 'react';
+import {Dimensions, StyleSheet} from 'react-native';
 
 export default function AnimatedStyleUpdateExample(props) {
   const translateX = useSharedValue(0);
@@ -20,25 +21,70 @@ export default function AnimatedStyleUpdateExample(props) {
     };
   });
 
+  const screenWidth = Dimensions.get('window').width;
+  const screenHeight = Dimensions.get('window').height;
+
+  const styles = StyleSheet.create({
+    box: {
+      width: 80,
+      height: 80,
+      backgroundColor: 'black',
+      marginTop: 35,
+      marginBottom: 10,
+      marginRight: 0,
+      marginLeft: 0,
+    },
+  });
+
   const gestureHandler = useAnimatedGestureHandler({
     onStart: (event, context) => {
       context.startX = translateX.value;
       context.startY = translateY.value;
     },
     onActive: (event, context) => {
-      translateX.value = withSpring(context.startX + event.translationX);
-      translateY.value = withSpring(context.startY + event.translationY);
+      const fixXPosition = (wantedX) => {
+        if (
+          wantedX >
+          screenWidth -
+            (styles.box.width + styles.box.marginLeft + styles.box.marginRight)
+        ) {
+          wantedX =
+            screenWidth -
+            (styles.box.width + styles.box.marginRight + styles.box.marginLeft);
+        } else if (wantedX < 0) {
+          wantedX = 0;
+        }
+        return wantedX;
+      };
+      const fixYPosition = (wantedY) => {
+        if (
+          wantedY >
+          screenHeight -
+            (styles.box.height + styles.box.marginTop + styles.box.marginBottom)
+        ) {
+          wantedY =
+            screenHeight -
+            (styles.box.height +
+              styles.box.marginTop +
+              styles.box.marginBottom);
+        } else if (wantedY < 0) {
+          wantedY = 0;
+        }
+        return wantedY;
+      };
+
+      translateX.value = withSpring(
+        fixXPosition(context.startX + event.translationX),
+      );
+      translateY.value = withSpring(
+        fixYPosition(context.startY + event.translationY),
+      );
     },
   });
 
   return (
     <PanGestureHandler onGestureEvent={gestureHandler}>
-      <Animated.View
-        style={[
-          {width: 80, height: 80, backgroundColor: 'black', margin: 30},
-          animatedStyles,
-        ]}
-      />
+      <Animated.View style={[styles.box, animatedStyles]} />
     </PanGestureHandler>
   );
 }
